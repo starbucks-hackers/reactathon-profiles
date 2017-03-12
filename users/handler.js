899ce4acdc2cc2ce2fee6a48da493c3dd3ea63a6
Brian Muse <bmuse@pdiarm.com>
2017-03-12 13:05:59 -0700

    Added user list functionality.

--- users/handler.js
+++ users/handler.js
@@ -11,21 +11,14 @@ module.exports.get = (event, context, callback) => {
   var userId = event.pathParameters.id;
   get.get(table, userId, callback);
 }
+module.exports.list = (event, context, callback) => {
+  list.list(table,callback);
+}
 module.exports.create = (event, context, callback) => {
   const data = JSON.parse(event.body);
-  /*if (typeof data.text !== 'string') {
-    console.error('Validation Failed');
-    callback(new Error('Couldn\'t create the user.'));
-    return;
-  }*/
   create.create(table, data, callback);
 }
 module.exports.delete = (event, context, callback) => {
   const data = JSON.parse(event.body);
-  /*if (typeof data.text !== 'string') {
-    console.error('Validation Failed');
-    callback(new Error('Couldn\'t create the user.'));
-    return;
-  }*/
   deleteProvider.delete(table, data.id, callback);
 }
